e5e19e07d54356328a4a6a14f3fdb90b09b1a95f
shuhanmirza <shuhan.mirza@gmail.com>
2020-07-16 19:20:42 +0600

    bug in song download. mp3-ogg in try-except

--- core/SongDownload.py
+++ core/SongDownload.py
@@ -12,16 +12,19 @@ def downloadOgg(music):
     if(url.find("https://www.youtube.com/watch") == 0):
         try:
             YoutubeUtils.downloadMp3(url.replace("https://www.youtube.com/watch?v=",""))
+            
+            AudioSegment.from_mp3(Constant.default_mp3_download_path).export(Constant.default_ogg_download_path, format='ogg',tags={'artist': music['artist'], 'title': music['title']})
+            
+            print(TAG,"completed...")
+            
+            return True
+
         except Exception as ex:
             print(TAG,ex)
             print(TAG, "ERROR => ", music)
             return False
 
-
-    AudioSegment.from_mp3(Constant.default_mp3_download_path).export(Constant.default_ogg_download_path, format='ogg',tags={'artist': music['artist'], 'title': music['title']})
-
-    print(TAG,"completed...")
-    return True
+    return False
 
     
         
