76091e3cde329c3a4afa80265c292f05477d3047
meafs <abrar.fahim@yandex.com>
2021-04-15 20:11:57 +0600

    Fix file_checker

--- core/content/file_checker.sh
+++ core/content/file_checker.sh
@@ -3,5 +3,5 @@
 while ! inotifywait -e modify program_schedule.csv; do 
 	pkill -f start.sh
 	pkill -f Main.py
-	bash /home/ubuntu/Workspace/sustcast-core-v0/startTest.sh &
+	bash /home/ubuntu/Workspace/sustcast-core-v0/start.sh &
 done
